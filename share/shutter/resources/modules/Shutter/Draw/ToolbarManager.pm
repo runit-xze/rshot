518fce83d29fe717656701684490e2f27bdab43a
abrookstgz-spec <286165832+abrookstgz-spec@users.noreply.github.com>
2026-06-24 20:40:18 -0700

    Extract Draw::CropPanel role from ToolbarManager
    - Create Shutter::Draw::CropPanel role hosting setup_right_vbox_c
      and adjust_crop_values
    - Consume the role in ToolbarManager via 'with'
    - Regenerate progressive critic baseline
    
    ToolbarManager: 757 -> 591 lines

--- share/shutter/resources/modules/Shutter/Draw/ToolbarManager.pm
+++ share/shutter/resources/modules/Shutter/Draw/ToolbarManager.pm
@@ -11,6 +11,7 @@ has drawing_tool => (is => 'ro', required => 1);
 
 with 'Shutter::Draw::Tool::ModeManager';
 with 'Shutter::Draw::ZoomControl';
+with 'Shutter::Draw::CropPanel';
 
 sub setup_bottom_hbox {
 	my $self = shift;
@@ -152,171 +153,7 @@ sub setup_bottom_hbox {
     return $drawing_bottom_hbox;
 }
 
-sub setup_right_vbox_c {
-	my $self = shift;
-	my $app = $self->drawing_tool;
 
-	my $cropping_bottom_vbox = Gtk3::VBox->new(FALSE, 5);
-
-	#get current pixbuf
-	my $pixbuf = $app->{_view}->get_pixbuf || $app->{_drawing_pixbuf};
-
-	my $value_callback = sub {
-		$app->{_selector}->set_selection({x=>$app->{_x_spin_w}->get_value, y=>$app->{_y_spin_w}->get_value, width=>$app->{_width_spin_w}->get_value, height=>$app->{_height_spin_w}->get_value});
-	};
-
-	#X
-	my $xw_label = Gtk3::Label->new($app->{_d}->get("X") . ":");
-	$app->{_x_spin_w} = Gtk3::SpinButton->new_with_range(0, $pixbuf->get_width, 1);
-	$app->{_x_spin_w}->set_value(0);
-	$app->{_x_spin_w_handler} = $app->{_x_spin_w}->signal_connect(
-		'value-changed' => $value_callback);
-
-	my $xw_hbox = Gtk3::HBox->new(FALSE, 5);
-	$xw_hbox->pack_start($xw_label,          FALSE, FALSE, 5);
-	$xw_hbox->pack_start($app->{_x_spin_w}, FALSE, FALSE, 5);
-
-	#y
-	my $yw_label = Gtk3::Label->new($app->{_d}->get("Y") . ":");
-	$app->{_y_spin_w} = Gtk3::SpinButton->new_with_range(0, $pixbuf->get_height, 1);
-	$app->{_y_spin_w}->set_value(0);
-	$app->{_y_spin_w_handler} = $app->{_y_spin_w}->signal_connect(
-		'value-changed' => $value_callback);
-
-	my $yw_hbox = Gtk3::HBox->new(FALSE, 5);
-	$yw_hbox->pack_start($yw_label,          FALSE, FALSE, 5);
-	$yw_hbox->pack_start($app->{_y_spin_w}, FALSE, FALSE, 5);
-
-	#width
-	my $widthw_label = Gtk3::Label->new($app->{_d}->get("Width") . ":");
-	$app->{_width_spin_w} = Gtk3::SpinButton->new_with_range(0, $pixbuf->get_width, 1);
-	$app->{_width_spin_w}->set_value(0);
-	$app->{_width_spin_w_handler} = $app->{_width_spin_w}->signal_connect(
-		'value-changed' => $value_callback);
-
-	my $ww_hbox = Gtk3::HBox->new(FALSE, 5);
-	$ww_hbox->pack_start($widthw_label,          FALSE, FALSE, 5);
-	$ww_hbox->pack_start($app->{_width_spin_w}, FALSE, FALSE, 5);
-
-	#height
-	my $heightw_label = Gtk3::Label->new($app->{_d}->get("Height") . ":");
-	$app->{_height_spin_w} = Gtk3::SpinButton->new_with_range(0, $pixbuf->get_height, 1);
-	$app->{_height_spin_w}->set_value(0);
-	$app->{_height_spin_w_handler} = $app->{_height_spin_w}->signal_connect(
-		'value-changed' => $value_callback);
-
-	my $hw_hbox = Gtk3::HBox->new(FALSE, 5);
-	$hw_hbox->pack_start($heightw_label,          FALSE, FALSE, 5);
-	$hw_hbox->pack_start($app->{_height_spin_w}, FALSE, FALSE, 5);
-
-	#the above values are changed when the selection is changed
-	$app->{_selector_handler} = $app->{_selector}->signal_connect(
-		'selection-changed' => sub {
-			$app->adjust_crop_values($pixbuf);
-		});
-
-	#cancel button
-	my $crop_c = Gtk3::Button->new_from_stock('gtk-cancel');
-	$crop_c->signal_connect('clicked' => sub { $app->abort_current_mode });
-
-	#crop button
-	my $crop_ok = Gtk3::Button->new_with_mnemonic($app->{_d}->get("_Crop"));
-	$crop_ok->set_image(Gtk3::Image->new_from_file($app->{_dicons} . '/transform-crop.png'));
-	$crop_ok->signal_connect(
-		'clicked' => sub {
-
-			my $s = $app->{_selector}->get_selection;
-			my $p = $app->{_view}->get_pixbuf;
-
-			if ($s && $p) {
-
-				#add to undo stack
-				$app->store_to_xdo_stack($app->{_canvas_bg}, 'modify', 'undo', $s);
-
-				#create new pixbuf
-				#create temp pixbuf because selected area might be bigger than
-				#source pixbuf (screenshot) => canvas area is resizeable
-				my $temp = Gtk3::Gdk::Pixbuf->new($app->{_drawing_pixbuf}->get_colorspace, TRUE, 8, $p->get_width, $p->get_height);
-
-				#whole pixbuf is transparent
-				$temp->fill(0x00000000);
-
-				#copy source image to temp pixbuf (temp pixbuf's size == $app->{_view}->get_pixbuf)
-				$app->{_drawing_pixbuf}->copy_area(0, 0, $app->{_drawing_pixbuf}->get_width, $app->{_drawing_pixbuf}->get_height, $temp, 0, 0);
-
-				#and create a new subpixbuf from the temp pixbuf
-				my $new_p = $temp->new_subpixbuf($s->{x}, $s->{y}, $s->{width}, $s->{height});
-				$app->{_drawing_pixbuf} = $new_p->copy;
-
-				#update bounds and bg_rects
-				$app->{_canvas_bg_rect}->set('width' => $s->{width}, 'height' => $s->{height});
-				$app->handle_bg_rects('update');
-
-				#update canvas and show the new pixbuf
-				$app->{_canvas_bg}->set('pixbuf' => $new_p);
-
-				#now move all items,
-				#so they are in the right position
-				#~ print $s->x ." - ".$s->y."\n";
-				$app->move_all($s->{x}, $s->{y});
-
-				#adjust stack order
-				$app->{_canvas_bg}->lower;
-				$app->{_canvas_bg_rect}->lower;
-				$app->handle_bg_rects('raise');
-
-			} else {
-
-				#nothing here right now
-			}
-
-			#finally reset mode to select tool
-			$app->abort_current_mode;
-
-		});
-
-	#put buttons in a separated box
-	#all buttons = one size
-	my $sg_butt = Gtk3::SizeGroup->new('vertical');
-	$sg_butt->add_widget($crop_c);
-	$sg_butt->add_widget($crop_ok);
-
-	my $cropping_bottom_vbox_b = Gtk3::VBox->new(FALSE, 5);
-	$cropping_bottom_vbox_b->pack_start($crop_c,  FALSE, FALSE, 0);
-	$cropping_bottom_vbox_b->pack_start($crop_ok, FALSE, FALSE, 0);
-
-	#final_packing
-	#all labels = one size
-	$xw_label->set_alignment(0, 0.5);
-	$yw_label->set_alignment(0, 0.5);
-	$widthw_label->set_alignment(0, 0.5);
-	$heightw_label->set_alignment(0, 0.5);
-
-	my $sg_main = Gtk3::SizeGroup->new('horizontal');
-	$sg_main->add_widget($xw_label);
-	$sg_main->add_widget($yw_label);
-	$sg_main->add_widget($widthw_label);
-	$sg_main->add_widget($heightw_label);
-
-	$cropping_bottom_vbox->pack_start($xw_hbox,                FALSE, FALSE, 3);
-	$cropping_bottom_vbox->pack_start($yw_hbox,                FALSE, FALSE, 3);
-	$cropping_bottom_vbox->pack_start($ww_hbox,                FALSE, FALSE, 3);
-	$cropping_bottom_vbox->pack_start($hw_hbox,                FALSE, FALSE, 3);
-	$cropping_bottom_vbox->pack_start($cropping_bottom_vbox_b, TRUE,  TRUE,  3);
-
-	#nice frame as well
-	my $crop_frame_label = Gtk3::Label->new;
-	$crop_frame_label->set_markup("<b>" . $app->{_d}->get("Selection") . "</b>");
-
-	my $crop_frame = Gtk3::Frame->new();
-	$crop_frame->set_border_width(5);
-	$crop_frame->set_label_widget($crop_frame_label);
-	$crop_frame->set_shadow_type('none');
-
-	$crop_frame->add($cropping_bottom_vbox);
-
-	return ($crop_frame, $crop_ok);
-}
 sub setup_view {
 	my $self = shift;
 	my $app = $self->drawing_tool;
@@ -747,11 +584,6 @@ sub adjust_rulers {
 }
 
 sub push_tool_help_to_statusbar {
-	my $self = shift;
-    return;
-}
-
-sub adjust_crop_values {
 	my $self = shift;
     return;
 }
